6b9b8a2ef7cc8311f393b865da8ba8482b13315f
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-06-11 20:52:12 +0000

    fix: remove semicolon after @Component and define AddToCartComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,30 @@ import { AddToCartComponent } from "./components/add-to-cart/add-to-cart.compone
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-});
+  styleUrls: ['./app.component.scss'], // ✅ plural and correct
+  standalone: true,
+  imports: [AddToCartComponent],
+})
+export class AppComponent {
+  title = 'Product list';
+  desserts: Dessert[] | null = null;
 
-// interface
+  constructor() {
+    this.desserts = desseretData;
+  }
+}
+
+// interfaces
 interface Dessert {
   image: DessertImages;
   name: string;
   category: string;
   price: number;
-};
+}
 
 interface DessertImages {
   thumbnail: string;
   mobile: string;
   tablet: string;
   desktop: string;
-};
-
-export class AppComponent {
-  title = 'Product list';
-  desserts:Dessert[] | null = null;
-
-  constructor() {
-    this.desserts = desseretData;
-  };
-};
+}
