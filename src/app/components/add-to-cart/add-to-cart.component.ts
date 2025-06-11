798e40940c1d60336842a32aa54e7a3881ab3cba
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-06-11 20:48:26 +0000

    @Component ended with a semicolon. - Used styleUrl instead of styleUrls

--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.component.html',
-  styleUrl: './add-to-cart.component.scss'
-});
-
+  styleUrls: ['./add-to-cart.component.scss'] 
+})
 export class AddToCartComponent {
   isAddedToCart = false;
   quantity = 1;
@@ -15,14 +14,13 @@ export class AddToCartComponent {
   }
 
   decreaseProductItem() {
-    if (this.quantity < 1) {
+    if (this.quantity <= 1) {
       this.isAddedToCart = false;
     }
-    this.quantity--;
+    this.quantity = Math.max(1, this.quantity - 1);
   }
 
   increaseProductItem() {
-    ++this.quantity;
+    this.quantity++;
   }
-
-};
+}
