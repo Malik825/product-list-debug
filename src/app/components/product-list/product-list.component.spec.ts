6580993cb113754d5d96f63c96b190dbe08dc1c4
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-06-12 13:28:12 +0000

    started testing

--- src/app/components/product-list/product-list.component.spec.ts
+++ src/app/components/product-list/product-list.component.spec.ts
@@ -1,6 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductListComponent } from './product-list.component';
+import { CartServiceService } from '../../services/cart-service.service';
+import { of } from 'rxjs';
+
+// Mock Dessert data
+const mockDesserts = [
+  {
+    name: 'Cake',
+    price: 10,
+    quantity: 1,
+    category: 'Baked',
+    image: {
+      thumbnail: 'thumb.jpg',
+      mobile: 'mobile.jpg',
+      tablet: 'tablet.jpg',
+      desktop: 'desktop.jpg'
+    }
+  }
+];
+
+// Mock CartService
+class MockCartService {
+  getAllProducts = jasmine.createSpy().and.returnValue(of(mockDesserts));
+}
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
@@ -8,9 +30,9 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductListComponent]
-    })
-    .compileComponents();
+      imports: [ProductListComponent],
+      providers: [{ provide: CartServiceService, useClass: MockCartService }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
--- src/app/order-confirmation-popup/order-confirmation-popup.component.spec.ts
+++ src/app/order-confirmation-popup/order-confirmation-popup.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OrderConfirmationPopupComponent } from './order-confirmation-popup.component';
+import { CartServiceService } from '../services/cart-service.service';
+import { of } from 'rxjs';
+
+class MockCartService {
+  clearCart = jasmine.createSpy();
+}
 
 describe('OrderConfirmationPopupComponent', () => {
   let component: OrderConfirmationPopupComponent;
@@ -8,12 +13,33 @@ describe('OrderConfirmationPopupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OrderConfirmationPopupComponent]
-    })
-    .compileComponents();
+      imports: [OrderConfirmationPopupComponent],
+      providers: [
+        { provide: CartServiceService, useClass: MockCartService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(OrderConfirmationPopupComponent);
     component = fixture.componentInstance;
+
+    // Provide mock @Input values
+    component.cartItems = [
+      {
+        name: 'Brownie',
+        price: 5,
+        quantity: 2,
+        category: 'Baked',
+        image: {
+          thumbnail: 'thumb.jpg',
+          mobile: 'mobile.jpg',
+          tablet: 'tablet.jpg',
+          desktop: 'desktop.jpg'
+        }
+      }
+    ];
+    component.totalPrice = 10;
+    component.isOpen = true;
+
     fixture.detectChanges();
   });
 
